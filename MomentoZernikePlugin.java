2a04ada5ff96d5014def4cfde278e258fb05b8d9
MayaPikachu <monteirolimamaya@gmail.com>
2024-06-16 17:51:43 -0300

    aiusbjdknam sdm´losa,.çd

--- MomentoZernikePlugin.java
+++ MomentoZernikePlugin.java
@@ -1,7 +1,9 @@
-/*Exemplo plugin para k-nearest
+package plugins;/*Exemplo plugin para k-nearest
    Prof. Joaquim Felipe */
 
 import java.io.*;
+import java.util.ArrayList;
+
 import ij.*;
 import ij.io.*;
 import ij.ImagePlus;
@@ -9,7 +11,8 @@ import ij.process.*;
 import ij.gui.*;
 import ij.plugin.filter.*;
 
-public class MomentoZernikePlugin implements PlugInFilter {
+
+public class _MomentoZernikePlugin implements PlugInFilter {
     ImagePlus reference;        // Reference image
     int k;                      // Number of nearest neighbors
     int level;                  // Wavelet decoposition level
@@ -23,29 +26,64 @@ public class MomentoZernikePlugin implements PlugInFilter {
 
     public void run(ImageProcessor img) {
 
-        GenericDialog gd = new GenericDialog("k-nearest neighbor search", IJ.getInstance());
-        gd.addNumericField("Number of nearest neighbors (K):", 1, 0);
-        gd.addNumericField("Wavelet decomposition level:", 1, 0);
+        ImageAccess[] imagemBase;
+        double nDist;
+        double[][] distancias;
+        ImagePlus reference;
+        int k;
+        int n;
+        String[] nomes;
+
+
+        GenericDialog gd = new GenericDialog("Momentos de Zernike", IJ.getInstance());
+        gd.addNumericField("Número 'k' de vizinhos a serem buscados: ", 5, 0);
+        gd.addNumericField("Grau máximo 'n' do polinômio a ser utilizado: ", 5, 0);
+        gd.addChoice("Função de distancia usada: ", new String[] { "Manhattan ", "Euclidiana", "Chebyshev"}, "Euclidiana");
         gd.showDialog();
         if (gd.wasCanceled())
             return;
         k = (int) gd.getNextNumber();
-        level = (int) gd.getNextNumber();
+        n = (int) gd.getNextNumber();
+        String distancia = gd.getNextChoice();
 
-        SaveDialog sd = new SaveDialog("Open search folder...", "any file (required)", "");
+        DistanceCalculator distanceCalculator;
+        switch (distancia) {
+            case "Euclidiana":
+                distanceCalculator = new DistanciaEuclidiana();
+                break;
+            case "Manhattan":
+                distanceCalculator = new DistanciaManhattan();
+                break;
+            case "Chebyshev":
+                distanceCalculator = new DistanciaChebyshev();
+                break;
+        }
+
+        SaveDialog sd = new SaveDialog("Escolha seu diretório", "Algum arquivo (necessário)", "");
         if (sd.getFileName()==null) return;
         String dir = sd.getDirectory();
 
-        search(dir);
+
+        ArrayList<double[]> caracteristicas;
+        try {
+            caracteristicas = caracteristicasDiretorio(dir, n);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void search(String dir) {
+    public ArrayList<double[]> caracteristicasDiretorio(String dir, int n) throws Exception{
         IJ.log("");
         IJ.log("Searching images");
+
         if (!dir.endsWith(File.separator))
             dir += File.separator;
+
         String[] list = new File(dir).list();  /* lista de arquivos */
-        if (list==null) return;
+        if (list == null) throw new Exception("Erro: diretorio vazio.");
+
+        ArrayList<double[]> vetoresCaracteristicas = new ArrayList<>();
+
         for (int i=0; i<list.length; i++) {
             IJ.showStatus(i+"/"+list.length+": "+list[i]);   /* mostra na interface */
             IJ.showProgress((double)i / list.length);  /* barra de progresso */
@@ -54,14 +92,94 @@ public class MomentoZernikePlugin implements PlugInFilter {
                 ImagePlus image = new Opener().openImage(dir, list[i]); /* abre imagem image */
                 if (image != null) {
 
-		            zernike = new Zernike();
-
-
+                    // CODIGO para inverter a imagem:
+                    ImageAccess input = new ImageAccess(image.getProcessor());
+                    int nx = input.getWidth();
+                    int ny = input.getHeight();
+                    vetoresCaracteristicas.add(Zernike.getFeatures(n, nx, ny, input));
                 }
             }
         }
-
         IJ.showProgress(1.0);
-        IJ.showStatus("");      
-     }      
+        IJ.showStatus("");
+
+        return vetoresCaracteristicas;
+    }
+
+    private class ImageData {
+        private double[] data;
+        private String imageName;
+
+        public ImageData(double[] data, String imageNAme) {
+
+        }
+
+        public double[] getData() {
+            return data;
+        }
+    }
+
+    private void saveToCsv(ArrayList<double[]> data, String filename) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename)))
+        {
+            for (double[] row : data) {
+                writer.print(imageNames[j] + ",");
+
+                for (int i = 0; i < row.length; i++) {
+                    writer.print(row[i]);
+                    if (i < row.length - 1) writer.print(",");
+                }
+            }
+            for (int j = 0; j < caracteristicas.length; j++) {
+                double[] row = caracteristicas[j];
+
+                for (int i = 0; i < row.length; i++) {
+                    writer.print(row[i]);
+                    if (i < row.length - 1) writer.print(",");
+                }
+                writer.println();
+            }
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            IJ.log("\n" + e.getMessage());
+        }
+    }
+
+    private interface DistanceCalculator {
+        double distance(double[] p1, double[] p2);
+    }
+
+    private class DistanciaManhattan implements DistanceCalculator {
+        @Override
+        public double distance(double[] p1, double[] p2) {
+            double dist = 0.0;
+            for (int i=0;i<Math.min(p1.length,p2.length);i++) {
+                dist += Math.abs(p1[i]-p2[i]);
+            }
+            return dist;
+        }
+    }
+
+    private class DistanciaEuclidiana implements DistanceCalculator {
+        @Override
+        public double distance(double[] p1, double[] p2) {
+            double dist = 0.0;
+            for (int i=0;i<Math.min(p1.length,p2.length);i++) {
+                dist += (p1[i]-p2[i])*(p1[i]-p2[i]);
+            }
+            return Math.sqrt(dist);
+        }
+    }
+    private class DistanciaChebyshev implements DistanceCalculator {
+        @Override
+        public double distance(double[] p1, double[] p2) {
+            double dist = 0.0;
+            for (int i=0;i<Math.min(p1.length,p2.length);i++) {
+                dist = Math.max(dist, Math.abs(p1[i]-p2[i]));
+            }
+            return dist;
+        }
+    }
 }
--- Zernike.java
+++ Zernike.java
@@ -1,22 +1,23 @@
+package plugins;
+
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Zernike {
 
-    public Zernike() {
-    }
+	public Zernike() {
+	}
 
 	// Function to calculate the radial component of the Zernike polynomial
 	private static double rnl(int n, int l, double rho) {
 		double sum = 0.0;
-			for (int s = 0; s <= (n - Math.abs(l)) / 2; s++) {
-				sum += Math.pow(-1, s) * fatorial(n - s)
-						/ (fatorial(s) * fatorial((n + Math.abs(l)) / 2 - s)
-						* fatorial((n - Math.abs(l)) / 2 - s))
-						* Math.pow(rho, n - 2 * s);
-			}
+		for (int s = 0; s <= (n - Math.abs(l)) / 2; s++) {
+			sum += Math.pow(-1, s) * fatorial(n - s)
+					/ (fatorial(s) * fatorial((n + Math.abs(l)) / 2 - s)
+					* fatorial((n - Math.abs(l)) / 2 - s))
+					* Math.pow(rho, n - 2 * s);
+		}
 		return sum;
 	}
 
@@ -39,7 +40,7 @@ public class Zernike {
 	}
 
 	private static double vnl(double theta, int l, int n, double radius){
-        return Math.sqrt((Math.pow(rnl(n, l, radius)*cosZernike(l, theta), 2)) + (Math.pow(rnl(n, l, radius)*sinZernike(l, theta), 2)));
+		return Math.sqrt((Math.pow(rnl(n, l, radius)*cosZernike(l, theta), 2)) + (Math.pow(rnl(n, l, radius)*sinZernike(l, theta), 2)));
 	}
 
 	// Function to convert Cartesian coordinates to polar coordinates
@@ -50,33 +51,49 @@ public class Zernike {
 	}
 
 	// Function to calculate the Zernike polynomial value at a given pixel
-	private static double znl(int n, int width, int height, int centerX, int centerY, BufferedImage image) {
+	private static double znl(int n, int l, int width, int height, ImageAccess image) {
 		double sum = 0.0;
-		int l;
-			for (int y = 0; y < height; y++) {
-				for (int x = 0; x < width; x++) {
-                    double[] polar = cartesianToPolar(x, y, centerX, centerY);
-                    double radius = polar[0] / Math.min(width, height); // Normalizing the radius
-                    double theta = polar[1];
-                    for (int i = n; i >= -n; i -= 2){
-						l = i;
-                        sum += ((n + 1) / Math.PI) * vnl(theta, l, n, radius)* image.getRGB(x, y);
-                    }
-                }
+		double radius = (double) Math.min(width, height) /2;
+		for(double r = 0; r < radius; r++) {
+			for(double theta = 0; theta < 2*Math.PI; theta+= 0.01){
+				sum += ((n + 1) / Math.PI) * vnl(theta, l, n, (r/radius)) * Math.ceil(image.getPixel((int) (r*Math.cos(theta)), (int) (r*Math.sin(theta)))/255)*r*(0.01);
 			}
+		}
 		return sum;
+	}
+
+	private static double[] znls(int n, int width, int height, ImageAccess image){
+		double result = 0.0;
+		double[] arr = new double[n+1];
+		int i = 0;
+		for(int l = -n; l <= n; l+=2){
+			arr[i] = znl(n, l, width, height, image);
+			i++;
+		}
+		return arr;
+	}
+
+	public static double[] getFeatures(int nMax, int width, int height, ImageAccess image){
+		double[] arr = new double[(nMax+2)*(nMax+1)/2];
+		int i = 0;
+		for(int n = 0; n <= nMax; n++){
+			double[] temp = znls(n, width, height, image);
+			for(int j = 0; j <=n; j++){
+				arr[i] = temp[j];
+				i++;
+			}
+		}
+		return arr;
 	}
 
 	public static void main(String[] args) {
 		try {
-			BufferedImage image = ImageIO.read(new File("input_image.jpg"));
+			ImageAccess image = ImageIO.read(new File("input_image.jpg"));
 			int width = image.getWidth();
 			int height = image.getHeight();
-			int centerX = width / 2;
-			int centerY = height / 2;
 			int n = 5;
-			double zernikeValue = znl(n, width, height, centerX, centerY, image);
-			System.out.println("Zernike Polynomial Value: " + zernikeValue);
+			double[] vetorCaracterísticas = getFeatures(n, width, height, image);
+			System.out.println("Zernike Polynomial Value: " + vetorCaracterísticas);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
